f72737e9becfbcd14394a3cf78c867fcac98bd82
Philipp Gühring <pg@futureware.at>
2023-02-25 21:13:48 +0100

    Improved Verilog

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -29,13 +29,6 @@
  *-------------------------------------------------------------
  */
 
-module gf180_teststructures #(
-)(
-
-);
-
-endmodule
-
 module user_project_wrapper #(
     parameter BITS = 32
 )(
@@ -73,49 +66,14 @@ module user_project_wrapper #(
     output [2:0] user_irq
 );
 
-/*--------------------------------------*/
-/* User project is instantiated  here   */
-/*--------------------------------------*/
-
-/*
-user_proj_example mprj (
+(* keep *)
+gf180_teststructures myteststructures (
 `ifdef USE_POWER_PINS
 	.vdd(vdd),	// User area 1 1.8V power
 	.vss(vss),	// User area 1 digital ground
 `endif
-
-    .wb_clk_i(wb_clk_i),
-    .wb_rst_i(wb_rst_i),
-
-    // MGMT SoC Wishbone Slave
-
-    .wbs_cyc_i(wbs_cyc_i),
-    .wbs_stb_i(wbs_stb_i),
-    .wbs_we_i(wbs_we_i),
-    .wbs_sel_i(wbs_sel_i),
-    .wbs_adr_i(wbs_adr_i),
-    .wbs_dat_i(wbs_dat_i),
-    .wbs_ack_o(wbs_ack_o),
-    .wbs_dat_o(wbs_dat_o),
-
-    // Logic Analyzer
-
-    .la_data_in(la_data_in),
-    .la_data_out(la_data_out),
-    .la_oenb (la_oenb),
-
-    // IO Pads
-
-    .io_in (io_in),
-    .io_out(io_out),
-    .io_oeb(io_oeb),
-
-    // IRQ
-    .irq(user_irq)
+        .gpio(io_in[0])
 );
-*/
-
-(* keep *) gf180_teststructures myteststructures ();
 
 endmodule	// user_project_wrapper
 
