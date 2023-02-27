cb9fda931a9344a072cb17a537dd9fe179df25fd
Philipp Gühring <pg@futureware.at>
2023-02-27 12:17:57 +0100

    Painted the Ports

--- gf180_teststructures/lef.tcl
+++ gf180_teststructures/lef.tcl
@@ -1,7 +1,20 @@
 gds read gf180_teststructures.gds
 load gf180_teststructures
 
-box values 0 0 0 0
+box values 0 0 10 10
+paint m4
+label vdd
+port make
+
+box values 30 0 40 10
+paint m4
+label vss
+port make
+
+box values 60 0 70 10
+paint m4
+label gpio
+port make 
 
 port vdd class inout
 port vdd use power
